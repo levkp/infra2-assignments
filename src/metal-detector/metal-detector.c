717fcf7ea0539b1ff342efaefbfbb93c4a129046
Levente Kátai-Pál <levente.katai-pal@student.kdg.be>
2021-05-30 13:57:47 +0200

    Working on metal detector

--- src/metal-detector/metal-detector.c
+++ src/metal-detector/metal-detector.c
@@ -1,3 +1,5 @@
+#include <util/delay.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -13,58 +15,79 @@ void metal_detector(void)
     initDisplay();
     initUSART();
 
-    Field *f = init_field(3, 10, 100);
+    int level = 1;
+    int size = 3;
 
-    bool horizontal = true;
+    while (level <= MAX_LEVEL) {
 
+        bool horizontal = true;
+        Field *f = init_field(level, size, 100);
 
-    while (f->moves > 0) {
-
-        draw_field(f);
-
-        while (true) {
-
-            writeCharToSegment(0, horizontal ? 'H' : 'V');
-            
-            if (buttonPushed(1))
-                horizontal = !horizontal;
-
-            if (buttonPushed(0)) {
-                
-                if (horizontal && f->player[X] != 0) {
-                    f->cells[f->player[Y]][f->player[X]] = EMPTY;
-                    f->player[X]--;
-                    f->cells[f->player[Y]][f->player[X]] = PLAYER;
-                    break;
-
-                } else if (!horizontal && f->player[Y] != 0) {
-                    f->cells[f->player[Y]][f->player[X]] = EMPTY;
-                    f->player[Y]--;
-                    f->cells[f->player[Y]][f->player[X]] = PLAYER;
-                    break;
-                }
-
-            } else if (buttonPushed(2)) {
-
-                if (horizontal && f->player[X] != f->size - 1) {
-                    f->cells[f->player[Y]][f->player[X]] = EMPTY;
-                    f->player[X]++;
-                    f->cells[f->player[Y]][f->player[X]] = PLAYER;
-                    break;
-                } else if (!horizontal && f->player[Y] != f->size - 1) {
-                    f->cells[f->player[Y]][f->player[X]] = EMPTY;
-                    f->player[Y]++;
-                    f->cells[f->player[Y]][f->player[X]] = PLAYER;
-                    break;
-                }
-            }  
+        if (!f) {
+            printf("\nError: unable to allocate memory.");
+            break;
         }
 
-        f->moves--;
+        printf("\nLevel %d", level);
 
+        while (f->moves > 0) {
+            
+            draw_field(f);
+
+            while (true) {
+
+                writeCharToSegment(0, horizontal ? 'H' : 'V');
+                   
+                if (buttonPushed(1)) {
+                    while (buttonPushed(1));
+                    horizontal = !horizontal;
+                }
+
+                if (buttonPushed(0)) {
+
+                    while (buttonPushed(0));
+
+                    if (horizontal && f->player[X] != 0) {
+                        move(f, f->player[X] - 1, f->player[Y]);
+                        break;
+                    } else if (!horizontal && f->player[Y] != 0) {
+                        move(f, f->player[X], f->player[Y] - 1);
+                        break;
+                    }
+
+                } else if (buttonPushed(2)) {
+
+                    while (buttonPushed(2));
+
+                    if (horizontal && f->player[X] != f->size - 1) {
+                        move(f, f->player[X] + 1, f->player[Y]);
+                        break;
+                    } else if (!horizontal && f->player[Y] != f->size - 1) {
+                        move(f, f->player[X], f->player[Y] + 1);
+                        break;
+                    }
+                }  
+            }
+
+            if (f->treasure[Y] == f->player[Y] && f->treasure[X] == f->player[X]) {
+                printf("\nLevel completed!");
+                break;
+            }
+        }
+
+        free_field(f);
+ 
+        if (f->moves == 0) {
+            printf("You're out of moves!\n");
+            break;
+        }
+
+        level++;
+        size++;
     }
 
-    free_field(f);
+
+    printf("End");
 }
 
 Field *init_field(int level, int size, int seed)
@@ -105,7 +128,7 @@ Field *init_field(int level, int size, int seed)
                 }
             }
 
-            int walls = level;
+            int walls = level - 1;
 
             while (walls != 0) {
                 
@@ -127,6 +150,7 @@ Field *init_field(int level, int size, int seed)
 
 void draw_field(Field *f)
 {
+    printf("\n");
     for (int i = 0; i < 2 * f->size + 1; i++)
         printf("─");
     printf("\n");
@@ -135,8 +159,9 @@ void draw_field(Field *f)
         for(int j = 0; j < f->size; j++) {
             if (f->cells[i][j] == EMPTY)
                 printf(" -");
-            else if (f->cells[i][j] == TREASURE)
+            else if (f->cells[i][j] == TREASURE) {
                 printf(" O");
+            }
             else if (f->cells[i][j] == WALL)
                 printf(" |");    
             else
@@ -160,50 +185,16 @@ void free_field(Field *f)
 
 int distance(Field *f)
 {
-    // Manhattan distance - doesn't take walls into account
     return abs(f->player[1] - f->treasure[1]) + abs(f->player[0] - f->treasure[0]);
 }
 
-// void setnew(Field *f, int x, int y)
-// {
-//     if (!(f->cells[Y][X] == WALL)) {
-//         f->cells[f->player[Y]][f->player[X]] = EMPTY;
-//         f->player[X] = x;
-//         f->player[Y] = y;
-//         f->cells[y][x] = PLAYER;
-//     }
-// }
-
-// void init_field2(Field *f, int level, int size, int seed)
-// {
-//     srand(seed);
-
-//     f = malloc(sizeof(*f));
-
-//     if (f != NULL) {
-
-//         f->cells = malloc(size * sizeof(*f->cells));
-
-//         if (f->cells != NULL) {
-
-//             f->size = size > MAX_SIZE ? MAX_SIZE : size;
-//             f->level = level > MAX_LEVEL ? MAX_LEVEL : level;
-//             f->moves = 2 * f->size;
-//             f->player[0] = rand() % f->size;
-//             f->player[1] = rand() % f->size;
-//             f->treasure[0] = rand() % f->size;
-//             f->treasure[1] = rand() % f->size;
-
-//             for (int i = 0; i < f->size; i++) {
-
-//                 f->cells[i] = malloc(f->size * sizeof(*f->cells[i]));
-
-//                 if (f->cells[i] != NULL) {
-//                     for (int j = 0; j < f->size; j++) {
-//                         f->cells[i][j] = EMPTY;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
+void move(Field *f, int new_x, int new_y)
+{
+    if (!(f->cells[new_y][new_x] == WALL)) {
+        f->cells[f->player[Y]][f->player[X]] = EMPTY;
+        f->player[X] = new_x;
+        f->player[Y] = new_y;
+        f->cells[new_y][new_x] = PLAYER;
+        f->moves--;
+    }
+}
--- src/metal-detector/metal-detector.h
+++ src/metal-detector/metal-detector.h
@@ -1,6 +1,6 @@
 #define MIN_SIZE 3
 #define MAX_SIZE 10
-#define MAX_LEVEL 9
+#define MAX_LEVEL 10
 #define X 1
 #define Y 0
 
@@ -17,14 +17,13 @@ void metal_detector(void);
 
 Field *init_field(int level, int size, int seed);
 
-// void init_field2(Field *f, int level, int size, int seed);
-
 void initADC_md(void);
 
 void draw_field(Field *f);
 
 void free_field(Field *f);
 
+// Manhattan distance - doesn't take walls into account
 int distance(Field *f);
 
-//void setnew(Field *f, int x, int y);
+void move(Field *f, int new_x, int new_y);
